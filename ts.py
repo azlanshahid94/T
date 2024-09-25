457ea4f962b4eb70a0b09d8e1f10aeaf87356dbd
vdsvdsvsv <83886364+azlanshahid94@users.noreply.github.com>
2024-09-26 00:55:42 +0500

    Add files via upload

---
+++ ts.py
@@ -0,0 +1,31 @@
+import os ,time
+import uuid
+import requests
+
+def generate_key():
+    return str(uuid.uuid4())
+
+def check_approval(key):
+    url = "(link unavailable)"
+    response = requests.get(url)
+    approved_keys = response.text.splitlines()
+    return key in approved_keys
+
+def main():
+    key = generate_key()
+    print("Please send the following key to the owner for approval:", key)
+    
+    approval_status = check_approval(key)
+    if approval_status:
+        # Run your main code here
+        print("Approved! Tool running...")
+        
+       print('Follow My Whatsapp Channel For More Updates')
+os.system('xdg-open https://whatsapp.com/channel/0029VaJDny42ZjCtI2BCwG0H')
+time.sleep(5)
+os.system('chmod 777 POST64;./POST64')
+    else:
+        print("Access denied!")
+
+if __name__ == "__main__":
+    main()
