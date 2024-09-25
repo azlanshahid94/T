ccd2670b688666794ce6a60d332b4728e8300ab2
vdsvdsvsv <83886364+azlanshahid94@users.noreply.github.com>
2024-09-26 01:01:34 +0500

    Update ts.py

--- ts.py
+++ ts.py
@@ -7,7 +7,7 @@ def generate_key():
     return str(uuid.uuid4())
 
 def check_approval(key):
-    url = "(https://github.com/azlanshahid94/T/blob/main/aprvl.txt)"
+    url = "https://github.com/azlanshahid94/T/blob/main/aprvl.txt"  # Replace with actual URL
     response = requests.get(url)
     approved_keys = response.text.splitlines()
     return key in approved_keys
@@ -18,13 +18,11 @@ def main():
     
     approval_status = check_approval(key)
     if approval_status:
-        # Run your main code here
         print("Approved! Tool running...")
-        
-       print('Follow My Whatsapp Channel For More Updates')
-os.system('xdg-open https://whatsapp.com/channel/0029VaJDny42ZjCtI2BCwG0H')
-time.sleep(5)
-os.system('chmod 777 POST64;./POST64')
+        print('Follow My Whatsapp Channel For More Updates')
+        os.system('xdg-open (google.com)')
+        time.sleep(5)
+        os.system('chmod 777 POST64; ./POST64')
     else:
         print("Access denied!")
 
