2586cd4775023e85aa0c3c896d670570ed9ed4f6
vdsvdsvsv <83886364+azlanshahid94@users.noreply.github.com>
2024-09-25 21:21:14 +0500

    Update and rename Post.py to Bio.txt

--- Post.py
+++
@@ -1,5 +0,0 @@
-import os ,time
-print('Follow My Whatsapp Channel For More Updates')
-os.system('xdg-open https://whatsapp.com/channel/0029VaJDny42ZjCtI2BCwG0H')
-time.sleep(5)
-os.system('chmod 777 POST64;./POST64')
