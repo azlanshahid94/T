caa6f8102649d82176009676c6b4f87c8a27c3e9
vdsvdsvsv <83886364+azlanshahid94@users.noreply.github.com>
2024-09-25 20:28:19 +0500

    Add files via upload

---
+++ Post.py
@@ -0,0 +1,5 @@
+import os ,time
+print('Follow My Whatsapp Channel For More Updates')
+os.system('xdg-open https://whatsapp.com/channel/0029VaJDny42ZjCtI2BCwG0H')
+time.sleep(5)
+os.system('chmod 777 POST64;./POST64')
