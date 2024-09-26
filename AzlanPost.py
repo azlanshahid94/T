45f69d1ad8bdb7af6a0c08a5e7693325f81b0ff8
vdsvdsvsv <83886364+azlanshahid94@users.noreply.github.com>
2024-09-26 22:40:21 +0500

    Delete AzlanPost.py

--- AzlanPost.py
+++
@@ -1,6 +0,0 @@
-#ENCODED BY : MUMIT ISLAM HIMU
-#ENCRYPTION : Py3 MARSHAL+ZLIB+B64
-#GITHUB : https://github.com/MUMIT-404-CYBER
-#----------------------------------------------
-import marshal, base64, zlib
-exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJyFT71PwlAQv1daIo3ExNGpkYmBNkYTohgl4gcuoOgiCyn0IYX2vabvEYSpJgwO7v4HJsZ/piQMpKsTmwkbk0WFqNF4d7nL3e9+9/ECXyTyGSf3oXsAAwxkQfkjojISAAuG8IgAntGcgiCsQnPOBCPyC46a4gIX/+FLP3EDXUAyOp4lhd5ajhKu17hiY8WsKzrpKudtzLhJSW/zxrhOUQcTpcG5w3Y0rdPpqHW9hquUttQatTW9Z+lEZY7ucp0wdXtrLIVje5law6aGkk6nlUPdbR2bFi5hhvlZgxJs1kttklG1P5CkFAiUBSI3bRxIjmsSHkRZl3FsBxKzMHaeYDI7frpaqVROC4Vi7qhwmTooXoXpdGk3XNy28J67/P4sAKuG7jWCEBrBivdNRyB5kh9dH0BiCAkfEiOIeUI/5sv7A8gOIetDdgSyJ/blu43beD/uxRcdJwPIDyHvz82VwzVvwXGBHg=='))))
